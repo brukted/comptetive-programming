1bd17a5f862db31c075b9a7462941c97f88ede1a
Bruk Tedla <biruk.tedla93@gmail.com>
2021-12-17 14:46:29 +0300

    Max Number of K-Sum Pairs

---
+++ weeks/week2/day5/maxNumberOfK-SumPairs.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/max-number-of-k-sum-pairs/
+
+from typing import List
+
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        ops = 0
+        dic = {}
+        for i in nums:
+            if k - i in dic.keys() and dic[k-i] > 0:
+                dic[k-i] -= 1
+                ops += 1
+            elif i in dic.keys():
+                dic[i] += 1
+            else:
+                dic[i] = 1
+        return ops
