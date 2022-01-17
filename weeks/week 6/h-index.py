e28ba5c8431b29e8aa709b07598edd4c83b35d18
Bruk Tedla <biruk.tedla93@gmail.com>
2022-01-17 17:35:41 +0300

    https://leetcode.com/problems/h-index/

---
+++ weeks/week 6/h-index.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/h-index/
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse=True)
+        if citations[0] == 0:
+            return 0
+        for i in range(len(citations)):
+            if i == len(citations)-1:
+                return i+1
+            if citations[i+1] <= i+1:
+                return i+1
