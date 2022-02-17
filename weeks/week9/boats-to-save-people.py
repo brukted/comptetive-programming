c8d69b9fb54c73798b9b64de3245c5dddb5a8f3a
Bruk Tedla <biruk.tedla93@gmail.com>
2022-02-17 13:23:22 +0300

    https://leetcode.com/problems/boats-to-save-people/

---
+++ weeks/week9/boats-to-save-people.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        i = 0
+        j = len(people) - 1
+        boats = 0
+        while i < j:
+            if people[i] + people[j] > limit:
+                boats += 1
+                j -= 1
+            else:
+                boats += 1
+                j -= 1
+                i += 1
+        if i == j:
+            boats += 1
+        return boats
