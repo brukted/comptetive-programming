4ef9d7393b5df3949333875e7b1f4be5b1f07362
Bruk Tedla <40060643+brukted@users.noreply.github.com>
2023-06-03 10:39:42 +0300

    Added solution for Container With Most Water

---
+++ a2sv-companion-test/week1/container-with-most-water.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) -1
+        best = 0
+        while left != right:
+            best = max(best,(right-left) * min(height[left],height[right]))
+            if height[left] > height[right]:
+                right -= 1
+            else:
+                left += 1
+        return best
